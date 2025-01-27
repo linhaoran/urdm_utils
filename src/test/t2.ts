1e95a2247d854d8e93a419d0d9db64c43798260e
Lin Haoran <linhaoran@163.com>
2025-01-27 11:40:19 +0800

    修改问统一json文件前

--- src/test/t2.ts
+++ src/test/t2.ts
@@ -1,14 +1,43 @@
-interface Subjects {
-    id: number;
-    value: boolean
+// interface Subjects { id: number; value: boolean }
+
+// const subjects: Array<boolean> = Array(11).fill(true)
+
+// console.log(typeof subjects);
+// console.log( subjects);
+// subjects[5] = false
+
+// subjects.forEach((x, i) => {
+//     console.log(i, x)
+// })
+
+
+const baseUrl = 'http://localhost:3001/model';
+
+(async () => {
+    const a = await fetch(baseUrl + '/reports/d/')
+        .then(resp => resp.json())
+        .then(data => data.map(i => i.id))
+        .catch(error => console.error('Error fetching users:', error));
+    // initReports = Array.from(new Set(reports))
+    // console.log('initReports: ', reports);
+    console.log(a)
+    // return reports;
+})()
+
+
+async function getSubjects2() {
+
+    let a = await fetch(baseUrl + '/reports/d/')
+        .then(resp => resp.json())
+        .then(data => {
+            data.map(i => i.id)
+            return data;
+        })
+        .catch(error => console.error('Error fetching users:', error));
+    // initReports = Array.from(new Set(reports))
+    // console.log('initReports: ', reports);
+    console.log(a)
+    return a;
 }
 
-const subjects: Array<boolean> = Array(11).fill(true)
-
-console.log(typeof subjects);
-// console.log( subjects);
-subjects[5] = false
-
-subjects.forEach((x, i) => {
-    console.log(i, x)
-})
+let a = getSubjects2();
